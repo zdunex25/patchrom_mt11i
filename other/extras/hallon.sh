5bb8718e3290ea44f7e2e03cb38de07f6855bbba
ZduneX25 <z25@cm7.(none)>
2013-05-04 17:56:37 +0200

    update to miui v5

--- other/extras/hallon.sh
+++ other/extras/hallon.sh
@@ -1,16 +1,17 @@
 #!/system/bin/sh
 
 echo Installing hallon libs
-cp /system/usr/extras/hallon/*.so /system/lib
+cp -f /system/usr/extras/hallon/*.so /system/lib
 chmod 644 /system/lib/libcald_hal.so
 chmod 644 /system/lib/libcald_pal.so
 chmod 644 /system/lib/liboemcamera.so
-cp /system/usr/extras/hallon/*.dat /system/usr/semc/camera
+cp -f /system/usr/extras/hallon/*.dat /system/usr/semc/camera
 chmod -R 644 /system/usr/semc/camera/
 chmod 755 /system/usr/semc/camera/
 cat /system/build.prop | sed -e "s/MT11/MT15/g" \
 			| sed -e "s/Xperia Neo V/Xperia Neo/g" \
 			| sed -e "s/haida/hallon/g" > /system/build2.prop
-mv /system/build2.prop /system/build.prop
+cp -f /system/build2.prop /system/build.prop
+rm -f /system/build2.prop
 chmod 644 /system/build.prop
 echo Done, reboot your phone!
